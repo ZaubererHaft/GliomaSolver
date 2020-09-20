8d26c41d033a630c47a8e71623a1a87cbf28a9f3
= <=>
2020-09-20 13:29:06 +0200

    add workflow stub

--- tools/DataProcessing/VoxelRemover.py
+++ tools/DataProcessing/VoxelRemover.py
@@ -1,25 +1,62 @@
 #!/usr/bin/python3
 
 import SimpleITK as sitk
+import logging
 import sys
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-sys.argv  = ['', '/home/ludwig/Repositories/Study/KAP/code/GliomaInput/InputData/T1c.nii.gz']
+sys.argv  = ['', 
+'/home/ludwig/Repositories/Study/KAP/code/GliomaInput/InputData/FLAIR.nii.gz', 
+'/home/ludwig/Repositories/Study/KAP/code/GliomaInput/InputData/CSF.nii.gz',
+'/home/ludwig/output.nii.gz']
 
-if len(sys.argv) <= 1:
+#Reads a nifti file with the fiven filename
+def read_image(fileName):
+  reader = sitk.ImageFileReader()
+  reader.SetImageIO("NiftiImageIO")
+  reader.SetFileName(fileName)
+
+  return reader.Execute()
+
+#writes a nifti image to the specified output path
+def write_image(image, output_path):
+  writer = sitk.ImageFileWriter()
+  writer.SetFileName(output_path)
+  writer.SetImageIO("NiftiImageIO")
+  writer.Execute(image)
+
+#debugs image information
+def debug_image_information(image):
+  for k in image.GetMetaDataKeys():
+    v = image.GetMetaData(k)
+    logger.debug(f"{k} : {v}")
+
+#ToDo: Remove this information
+  logger.debug(image.GetSize())
+  logger.debug(image.GetOrigin())
+  logger.debug(image.GetSpacing())
+  logger.debug(image.GetDirection())
+  logger.debug(image.GetNumberOfComponentsPerPixel())
+  logger.debug(image.GetDimension())
+  logger.debug(image.GetPixelIDValue())
+  logger.debug(image.GetPixelIDTypeAsString())
+
+def remove_spatially_overlapping_voxels(csf_image, flair_image):
+  #ToDo: implement
+  return flair_image
+
+if len(sys.argv) <= 3:
     print('Missing argument')
     quit()
 
-inputData = sys.argv[1]
+flair_image = read_image(sys.argv[1])
+csf_image = read_image(sys.argv[2])
 
-reader = sitk.ImageFileReader()
-reader.SetImageIO("NiftiImageIO")
-reader.SetFileName(inputData)
-image = reader.Execute()
+debug_image_information(flair_image)
+debug_image_information(csf_image)
 
-#toto: remove nonzero voxels
+non_overlapping_flair_image = remove_spatially_overlapping_voxels(csf_image, flair_image)
 
-writer = sitk.ImageFileWriter()
-writer.SetFileName('/home/ludwig/my_output.nii.gz')
-writer.SetImageIO("NiftiImageIO")
-writer.Execute(image)
+write_image(non_overlapping_flair_image, sys.argv[3])
