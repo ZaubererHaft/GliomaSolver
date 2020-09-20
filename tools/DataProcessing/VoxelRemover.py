2ca2c41040534c3058231fa36c5dba70f25d3a98
= <=>
2020-09-20 18:40:30 +0200

    add naive implementation of overlapping deletion

--- tools/DataProcessing/VoxelRemover.py
+++ tools/DataProcessing/VoxelRemover.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 sys.argv  = ['', 
-'/home/ludwig/Repositories/Study/KAP/code/GliomaInput/InputData/FLAIR.nii.gz', 
+'/home/ludwig/Repositories/Study/KAP/code/GliomaInput/InputData/Tum_FLAIR.nii.gz', 
 '/home/ludwig/Repositories/Study/KAP/code/GliomaInput/InputData/CSF.nii.gz',
 '/home/ludwig/output.nii.gz']
 
@@ -32,23 +32,26 @@ def debug_image_information(image):
   for k in image.GetMetaDataKeys():
     v = image.GetMetaData(k)
     logger.debug(f"{k} : {v}")
+  logger.debug("--------------------------------")
 
-#ToDo: Remove this information
-  logger.debug(image.GetSize())
-  logger.debug(image.GetOrigin())
-  logger.debug(image.GetSpacing())
-  logger.debug(image.GetDirection())
-  logger.debug(image.GetNumberOfComponentsPerPixel())
-  logger.debug(image.GetDimension())
-  logger.debug(image.GetPixelIDValue())
-  logger.debug(image.GetPixelIDTypeAsString())
-
+#naive implementation to remove overlapping voxels of two nii images
 def remove_spatially_overlapping_voxels(csf_image, flair_image):
-  #ToDo: implement
-  return flair_image
+  for x in range(flair_image.GetSize()[0]):
+    for y in range(flair_image.GetSize()[1]):
+      for z in range(flair_image.GetSize()[2]):
+
+        flair_pixel = flair_image[x,y,z]
+        csf_pixel = csf_image[x,y,z]
+        
+        #overlap
+        if flair_pixel > 0 and csf_pixel > 0:
+          logger.debug(f"found overlap at ({x},{y},{z}) because pixel value at flair is {flair_pixel} and at csf is {csf_pixel}")
+          csf_image[x,y,z] = 0
+
+  return csf_image
 
 if len(sys.argv) <= 3:
-    print('Missing argument')
+    print("Missing argument")
     quit()
 
 flair_image = read_image(sys.argv[1])
@@ -57,6 +60,6 @@ csf_image = read_image(sys.argv[2])
 debug_image_information(flair_image)
 debug_image_information(csf_image)
 
-non_overlapping_flair_image = remove_spatially_overlapping_voxels(csf_image, flair_image)
+non_overlapping_csf_image = remove_spatially_overlapping_voxels(csf_image, flair_image)
 
-write_image(non_overlapping_flair_image, sys.argv[3])
+write_image(non_overlapping_csf_image, sys.argv[3])
