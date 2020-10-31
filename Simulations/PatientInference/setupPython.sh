19fd73eb0ab29187cabe8fd2517ee23e84392701
Ludwig Kratzl <kratzl.ludwig@gmail.com>
2020-10-31 17:51:15 +0100

    Remove -c flag

--- Simulations/PatientInference/setupPython.sh
+++ Simulations/PatientInference/setupPython.sh
@@ -6,5 +6,5 @@ conda create -n py38 python=3.8
 source activate py38
 conda install -c anaconda numpy
 conda install -c simpleitk simpleitk
-conda install -c scikit-learn
+conda install scikit-learn
 echo "done"
